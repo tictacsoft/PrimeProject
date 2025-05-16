0e4a95723a3fcc9b14b8bc190a4d2a90bc4a83ec
unknown <projectlaravel71@gmail.com>
2025-05-16 18:02:11 +0700

    Merubah Form Register

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layout.master')
 @section('title', 'Register')
 @section('content')
-    <div class="slider-area ">
+    {{-- <div class="slider-area ">
         <div class="single-slider section-overly slider-height2 d-flex align-items-center"
             data-background="{{ asset('assets/img/hero/about.jpg') }}">
             <div class="container">
@@ -14,37 +14,37 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <section class="contact-section ">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="row">
                 <div class="col-12">
                     <h2 class="contact-title text-center">Register</h2>
                 </div>
-                <div class="col-4">
+                <div class="col-12 col-md-4 mb-3">
                     <a href="/register/freelancer" class="text-decoration-none">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://i.pinimg.com/736x/d9/b7/02/d9b70237f2569ba9dd5f386d16d95dab.jpg" class="card-img-top mx-auto" alt="..." width="50">
+                        <div class="card mx-auto shadow" style="width: 18rem;">
+                            <img src="https://i.pinimg.com/736x/d9/b7/02/d9b70237f2569ba9dd5f386d16d95dab.jpg" class="card-img-top mx-auto" alt="..." height="250" width="50">
                             <div class="card-body">
                                 <h5 class="card-title text-center">Freelancer</h5>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-12 col-md-4 mb-3">
                     <a href="/register/clients" class="text-decoration-none">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://i.pinimg.com/736x/17/7c/b0/177cb05010d6968079f394928575e3d5.jpg" class="card-img-top mx-auto" alt="..." width="50">
+                        <div class="card mx-auto shadow" style="width: 18rem;">
+                            <img src="https://i.pinimg.com/736x/17/7c/b0/177cb05010d6968079f394928575e3d5.jpg" class="card-img-top mx-auto" alt="..." height="250" width="50">
                             <div class="card-body">
                                 <h5 class="card-title text-center">Clients</h5>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-12 col-md-4 mb-3">
                     <a href="/register/company" class="text-decoration-none">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://i.pinimg.com/736x/d7/8f/8f/d78f8fa0cee7f78253a7a309ac5c408a.jpg" class="card-img-top mx-auto" alt="..." width="50">
+                        <div class="card mx-auto shadow" style="width: 18rem;">
+                            <img src="https://i.pinimg.com/736x/d7/8f/8f/d78f8fa0cee7f78253a7a309ac5c408a.jpg" class="card-img-top mx-auto" alt="..." height="250" width="50">
                             <div class="card-body">
                                 <h5 class="card-title text-center">Company</h5>
                             </div>
