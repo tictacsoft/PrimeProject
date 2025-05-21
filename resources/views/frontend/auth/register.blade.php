34bea846d38f5ef0ad312a4053bf39e1b716df12
Mac <mac@Macs-MacBook-Pro.local>
2025-05-21 19:44:33 +0700

    Company changed to supplier

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -42,7 +42,7 @@
                     </a>
                 </div>
                 <div class="col-12 col-md-4 mb-3">
-                    <a href="/register/company" class="text-decoration-none">
+                    <a href="/register/supplier" class="text-decoration-none">
                         <div class="card mx-auto shadow" style="width: 18rem;">
                             <img src="https://i.pinimg.com/736x/d7/8f/8f/d78f8fa0cee7f78253a7a309ac5c408a.jpg" class="card-img-top mx-auto" alt="..." height="250" width="50">
                             <div class="card-body">
